22d3f99b71cd484b3ff5a55f4e79f74d48986df2
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2024-08-04 01:51:43 +0530

    few changes

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Navbar() {
     return (
         <div className="bg-slate-900 text-white h-10 flex justify-center items-center p-2">
-            <p>'Reflex Time' by @itsmeprinceyt</p>
+            <p>[Reflex Time] by ItsMe Prince</p>
             <div className="absolute right-2">
                 <Link href="https://github.com/itsmeprinceyt/reflex-time">Github</Link>
             </div>
